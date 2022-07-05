527ebc0d73a19fde30daa92a5dde5f01a56513ca
LauDAlvarez <lautarodalvarez@gmail.com>
2022-07-05 00:14:34 -0300

    actualizacion de mercado liebre

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+const { Console } = require('console');
 const express = require('express');
 const app = express();
 const path = require('path')
-app.use(express.static('public'));
 
+//MIDDLEWARDE
+app.use(express.static('publicPath'));
+app.set('puerto', process.env.PORT || 3000)
 
-app.listen(3000, ()=>{
-    console.log('Corriendo...');
-});
+//Rutas
+app.listen( app.get('puerto'), ()=> console.log(`Servidor corriendo en el ${app.get('puerto')}`));
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname + '/views/home.html'));
