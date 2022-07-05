dc4b6f65c713f58e43cf17a915c07a63c0e34307
LauDAlvarez <lautarodalvarez@gmail.com>
2022-07-05 00:22:06 -0300

    modificacion de puerto

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const { Console } = require('console');
 const express = require('express');
 const app = express();
+
 const path = require('path')
 
 //MIDDLEWARDE
-app.use(express.static('publicPath'));
+app.use(express.static('public'));
 app.set('puerto', process.env.PORT || 3000)
 
 //Rutas
-app.listen( app.get('puerto'), ()=> console.log(`Servidor corriendo en el ${app.get('puerto')}`));
+
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname + '/views/home.html'));
@@ -21,3 +21,4 @@ app.get('/register', (req, res)=>{
 app.get('/login', (req, res)=>{
     res.sendFile(path.join(__dirname + '/views/login.html'))
 })
+app.listen( app.get('puerto'), ()=> console.log(`Servidor corriendo en el ${app.get('puerto')}`));
